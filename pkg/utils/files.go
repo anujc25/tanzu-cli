0beef0ccb5b4cb86ad7e844343aec789a231751a
Anuj Chaudhari <anujc@vmware.com>
2023-05-14 18:01:00 -0700

    Add --plugin-inventory-db-file flag to inventory update commands
    - This change gives flexibility to update local database file instead of
      always downloading plugin inventory database file from remote location

--- pkg/utils/files.go
+++ pkg/utils/files.go
@@ -56,3 +56,16 @@ func PathExists(dir string) bool {
 	}
 	return true
 }
+
+// AppendFile appends data to the filePath. It creates the file if it doesn’t already exist.
+func AppendFile(filePath string, data []byte) error {
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, constants.ConfigFilePermissions)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if _, err := f.Write(data); err != nil {
+		return err
+	}
+	return nil
+}
